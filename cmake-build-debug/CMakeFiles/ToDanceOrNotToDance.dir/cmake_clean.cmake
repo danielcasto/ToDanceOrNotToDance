65b300e3a0d5fa9990a1d20597d7a10934b97539
danielcasto <32224738+danielcasto@users.noreply.github.com>
2021-03-14 21:54:50 -0400

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/ToDanceOrNotToDance.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ToDanceOrNotToDance.dir/main.cpp.obj"
+  "ToDanceOrNotToDance.exe"
+  "ToDanceOrNotToDance.exe.manifest"
+  "ToDanceOrNotToDance.lib"
+  "ToDanceOrNotToDance.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ToDanceOrNotToDance.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
