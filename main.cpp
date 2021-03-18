0ea29ddb6e60ef2ae3f57218f6c5411c2e7a61f7
JerryMbamo <jmbamo@yahoo.com>
2021-03-18 17:33:30 -0400

    sample interface

--- main.cpp
+++ main.cpp
@@ -1,8 +1,42 @@
 #include <iostream>
 
+using namespace std;
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    cout << "Welcome to Dance for Not to Dance! " << endl;
+    cout << "-----------------------------------" << endl;
+    cout << "How would you like the albums ranked on Danceability: " << endl;\
+    cout << "0. Exit " << endl;
+    cout << "1. Least -> Most " << endl;
+    cout << "2. Most -> Least " << endl;
+    cout << endl;
+    cout << "// Next menu after input " << endl;
+    cout << endl;
+    cout << "Would you like Albums contain explict songs? " << endl;
+    cout << "0. Exit " << endl;
+    cout << "1. Yes " << endl;
+    cout << "2. No " << endl;
+    cout << endl;
+    cout << "// Next menu after input " << endl;
+    cout << endl;
+    cout << "Any particular Artist? " << endl;
+    cout << "0. Exit " << endl;
+    cout << "1. Yes " << endl;
+    cout << "2. No " << endl;
+    cout << endl;
+    cout << "// Next menu after input, if they said yes to question above " << endl;
+    cout << endl;
+    cout << "Enter name of artist: " << endl;
+    cout << endl;
+    cout << "// Next menu after input, if they said yes to question above or if the said no " << endl;
+    cout << endl;
+    cout << "MAX number of tracks per Album (-1 for no max): " << endl;
+    cout << endl;
+    cout << "// Next menu after input " << endl;
+    cout << endl;
+    cout << "How many album results would you like to see? ( -1 for all results) " << endl;
+
+
 }
 // some random comment
 //test
