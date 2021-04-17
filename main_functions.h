4681dd72c8e721b9c7881cd7a35f279c12d79c26
KKKjj-son <56332772+KKKjj-son@users.noreply.github.com>
2021-04-17 15:51:24 -0400

    Update main_functions.h

--- main_functions.h
+++ main_functions.h
@@ -11,3 +11,6 @@ void heapify_down(vector<Album>& list, int size, int root);
 void heapify_minHeap(vector<Album>& list, int size, int index);
 void heapSortMax(vector<Album>& list, int size);
 void heapSortMin(vector<Album>& list, int size);
+
+int partition(vector<Album>& list, int low, int high);
+void quickSort(vector<Album>& list, int low, int high);
